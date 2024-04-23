992578d3b3ff4563bb6c650caf7446076302e603
Jongha <jongha1230@naver.com>
2024-04-23 11:17:37 +0900

    다크 모드 구현

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ const sortAndDisplayMovies = (movies) => {
     displayMovies(sortedMovies);
 };
 
+// 영화 리스트 초기화 함수
+const clearMovieList = (movieList) => {
+    movieList.innerHTML = "";
+};
+
 // 화면에 영화 목록 표시 함수
 function displayMovies(movies) {
     const movieList = document.getElementById("movie-list");
@@ -31,35 +36,6 @@ function displayMovies(movies) {
         const movieCard = createMovieCard(movie, index); // 영화 카드 생성
         movieList.appendChild(movieCard);
     });
-};
-
-// 영화 카드 생성 함수
-const createMovieCard = (movie, index) => {
-    const movieCard = document.createElement("div");
-    movieCard.classList.add("movie-card");
-    movieCard.id = `${movie.id}`;
-
-    const img = document.createElement("img");
-    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    img.alt = movie.title;
-
-    const title = createMovieTitle(movie.title); // 영화 타이틀 요소 생성
-    const content = createMovieContent(movie.overview); // 영화 소개 요소 생성
-    const rating = createMovieRating(movie.vote_average); // 영화 평점 요소 생성
-    const placeBadge = createPlaceBadge(index); // 1위 ~ 3위 영화 뱃지 생성
-
-    movieCard.append(img, title, rating, content);
-    if (index < 3) {
-        movieCard.appendChild(placeBadge);
-    }
-
-    // 영화 카드 클릭시 alert창이 뜨는 이벤트리스너
-    movieCard.addEventListener("click", () => {
-        const clickedCardId = movieCard.id;
-        window.alert(`ID: ${clickedCardId}`);
-    });
-
-    return movieCard;
 };
 
 // 영화 타이틀 요소 생성 함수
@@ -87,16 +63,41 @@ const createMovieRating = (voteAverage) => {
 };
 
 // 1위 ~ 3위 영화 뱃지 생성 함수
-const createPlaceBadge = (index) => {
-    const placeBadge = document.createElement('div');
-    placeBadge.classList.add('place-badge');
-    placeBadge.textContent = `${index + 1}st`;
-    return placeBadge;
-};
+// const createPlaceBadge = (index) => {
+//     const placeBadge = document.createElement('div');
+//     placeBadge.classList.add('place-badge');
+//     placeBadge.textContent = `${index + 1}st`;
+//     return placeBadge;
+// };
 
-// 영화 리스트 초기화 함수
-const clearMovieList = (movieList) => {
-    movieList.innerHTML = "";
+
+// 영화 카드 생성 함수
+const createMovieCard = (movie, index) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+    movieCard.id = `${movie.id}`;
+
+    const img = document.createElement("img");
+    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    img.alt = movie.title;
+
+    const title = createMovieTitle(movie.title); // 영화 타이틀 요소 생성
+    const content = createMovieContent(movie.overview); // 영화 소개 요소 생성
+    const rating = createMovieRating(movie.vote_average); // 영화 평점 요소 생성
+    // const placeBadge = createPlaceBadge(index); // 1위 ~ 3위 영화 뱃지 생성
+
+    movieCard.append(img, title, rating, content);
+    // if (index < 3) {
+    //     movieCard.appendChild(placeBadge);
+    // }
+
+    // // 영화 카드 클릭시 alert창이 뜨는 이벤트리스너
+    // movieCard.addEventListener("click", () => {
+    //     const clickedCardId = movieCard.id;
+    //     window.alert(`ID: ${clickedCardId}`);
+    // });
+
+    return movieCard;
 };
 
 // 검색 이벤트 리스너 
@@ -121,7 +122,6 @@ const searchInput = document.getElementById("search-input");
 
 // 메인 타이틀 클릭 시 페이지 새로고침 
 document.getElementById("header-title").addEventListener("click", () => {
-    searchInput.focus();
     location.reload();    
 });
 
@@ -130,7 +130,40 @@ document.getElementById("input-clear-btn").addEventListener("click", () => {
     searchInput.value = "";
 });
 
-// 페이지 로드 또는 새로고침 이벤트가 발생하면 검색 입력란에 포커스
-window.addEventListener("load", () => {
-    searchInput.focus();
+// 영화 리스트의 부모 요소에 클릭 이벤트 리스너 추가
+document.getElementById("movie-list").addEventListener("click", (event) => {
+    // 클릭된 요소가 영화 카드 또는 그 자식 요소인 경우에 처리
+    const movieCard = event.target.closest(".movie-card");
+    if (movieCard) {
+        const clickedCardId = movieCard.id;
+        window.alert(`ID: ${clickedCardId}`);
+    }
 });
+
+// 다크 모드 토글 함수
+const toggleDarkmode = () => {
+    const body = document.body;
+    const darkModeEnabled = !body.classList.contains('dark-mode');
+    body.classList.toggle('dark-mode');
+
+    // 다크 모드 상태를 localStorage에 저장
+    localStorage.setItem('darkModeEnabled', darkModeEnabled);
+};
+
+// 페이지 로드 시 localStorage에서 다크 모드 설정 가져오기
+window.addEventListener('DOMContentLoaded', () => {
+    const darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+    const darkmodeToggle = document.getElementById('dark-mode-toggle');
+    
+    // 다크 모드 설정이 저장된 경우에만 다크 모드를 활성화하고 체크박스를 업데이트합니다.
+    if (darkModeEnabled) {
+        toggleDarkmode(); 
+        darkmodeToggle.checked = false;
+    } else {
+        darkmodeToggle.checked = true;
+    }
+});
+
+// 다크 모드 토글 체크박스의 이벤트 리스너
+document.getElementById('dark-mode-toggle').addEventListener('change', toggleDarkmode);
+
